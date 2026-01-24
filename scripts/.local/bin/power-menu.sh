172df8f93d90ae14e2c8caddc7249a55b06fe73d
alaeddine119 <25179455+alaeddine119@users.noreply.github.com>
2026-01-24 22:57:56 +0100

    use rofi instead of fuzzel

--- scripts/.local/bin/power-menu.sh
+++ scripts/.local/bin/power-menu.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
-
-# Define the icons and labels
 lock="🔒 Lock"
 suspend="💤 Suspend"
-reboot="🔄 Reboot"
-shutdown="⏻ Shutdown"
 logout="🚪 Logout"
+reboot="♻️ Reboot"
+shutdown="🛑 Shutdown"
 
-# Show the menu using Fuzzel
-# -d: dmenu mode
-# -p: prompt text
-# -w: width (characters)
-# -l: lines (height)
-selected=$(echo -e "$lock\n$suspend\n$reboot\n$shutdown\n$logout" | fuzzel -d -p "Power: " -w 20 -l 5)
+# Rofi Command
+selected=$(echo -e "$lock\n$suspend\n$logout\n$reboot\n$shutdown" | rofi -dmenu -p "Power")
 
 case $selected in
-    "$lock")
-        swaylock -f --screenshots --clock --indicator --effect-scale 0.5 --effect-blur 7x5 --effect-scale 2 ;;
-    "$suspend")
-        systemctl suspend ;;
-    "$reboot")
-        systemctl reboot ;;
-    "$shutdown")
-        systemctl poweroff ;;
-    "$logout")
-        swaymsg exit ;;
+    "$lock") swaylock -f --screenshots --clock --indicator --effect-scale 0.5 --effect-blur 7x5 --effect-scale 2 ;;
+    "$suspend") systemctl suspend ;;
+    "$logout") swaymsg exit ;;
+    "$reboot") systemctl reboot ;;
+    "$shutdown") systemctl poweroff ;;
 esac
