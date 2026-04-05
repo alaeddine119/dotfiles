f6786bd334fd5926709ad89e079d6e071a85a1cf
alaeddine119 <25179455+alaeddine119@users.noreply.github.com>
2026-04-05 11:56:21 +0200

    default options

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -17,31 +17,6 @@ end
 -- 3. CONFIGURE
 markdown.setup({
 	-- basic configuration
-	file_types = { "markdown", "Avante", "codecompanion" },
+	file_types = { "markdown" },
 	render_modes = { "n", "c" }, -- Render in Normal and Command modes
-
-	heading = {
-		-- Turn the ## into a nice icon and background pill
-		sign = false, -- Disable sign column icons to keep it clean
-		icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-		position = "overlay", -- Hide the ## symbols
-		width = "full", -- Background color spans whole screen (Rose Pine style)
-	},
-
-	code = {
-		-- Make code blocks look like "blocks" with a subtle background
-		sign = false,
-		style = "language",
-		width = "block",
-		right_pad = 1,
-		border = "thick",
-	},
-
-	checkbox = {
-		enabled = true,
-		unchecked = { icon = "󰄱 " },
-		checked = { icon = "󰱒 " },
-	},
-
-	latex = { enabled = true },
 })
