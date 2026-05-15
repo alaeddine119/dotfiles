6880c2ebef021bb2633c9cb51d7a030a11e0c9eb
alaeddine119 <25179455+alaeddine119@users.noreply.github.com>
2026-05-15 02:29:55 +0200

    fix arabic in kitty

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -4,6 +4,7 @@ local lines = vim.api.nvim_buf_get_lines(0, 0, 50, false)
 for _, line in ipairs(lines) do
 	if line:match("[\216-\219][\128-\191]") then
 		vim.opt_local.arabic = true
+		vim.opt.arabicshape = false
 		vim.o.spell = false
 		break
 	end
