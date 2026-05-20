f15c4f295dcc56aa5ecaddffd0710c12cbb8eb80
alaeddine119 <25179455+alaeddine119@users.noreply.github.com>
2026-05-20 17:46:27 +0200

    added oil-git

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,17 +5,19 @@
 vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
 	"https://github.com/nvim-mini/mini.icons",
+	"https://github.com/malewicz1337/oil-git.nvim",
 })
 
 local ok, oil = pcall(require, "oil")
-if not ok then
+local ok_git, oil_git = pcall(require, "oil-git")
+if not ok or not ok_git then
 	return
 end
 
 local detail = false
 oil.setup({
 	view_options = { show_hidden = true },
-	win_options = { signcolumn = "yes:2" },
+	win_options = { signcolumn = "yes:1" },
 	keymaps = {
 		["gd"] = {
 			desc = "Toggle file detail view",
@@ -34,4 +36,13 @@ oil.setup({
 			end,
 		},
 	},
+})
+
+-- 4. INITIALIZE GIT EXTENSION SECOND
+oil_git.setup({
+	show_file_highlights = true,
+	show_directory_highlights = true,
+	show_ignored_files = false,
+	-- 💡 Route the status symbols into your sign column layout
+	symbol_position = "signcolumn",
 })
