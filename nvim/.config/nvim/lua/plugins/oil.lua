07a421a370c58a0f4ea16264492caef8ae17a403
alaeddine119 <25179455+alaeddine119@users.noreply.github.com>
2026-05-22 00:58:06 +0200

    add lsp extension

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -6,18 +6,20 @@ vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
 	"https://github.com/nvim-mini/mini.icons",
 	"https://github.com/malewicz1337/oil-git.nvim",
+	"https://github.com/JezerM/oil-lsp-diagnostics.nvim",
 })
 
 local ok, oil = pcall(require, "oil")
 local ok_git, oil_git = pcall(require, "oil-git")
-if not ok or not ok_git then
+local ok_lsp, oil_lsp = pcall(require, "oil-lsp-diagnostics")
+if not ok or not ok_git or not ok_lsp then
 	return
 end
 
 local detail = false
 oil.setup({
 	view_options = { show_hidden = true },
-	win_options = { signcolumn = "yes:1" },
+	win_options = { signcolumn = "yes:2" },
 	keymaps = {
 		["gd"] = {
 			desc = "Toggle file detail view",
@@ -38,11 +40,12 @@ oil.setup({
 	},
 })
 
--- 4. INITIALIZE GIT EXTENSION SECOND
+-- 4. INITIALIZE GIT EXTENSION
 oil_git.setup({
-	show_file_highlights = true,
-	show_directory_highlights = true,
-	show_ignored_files = false,
-	-- 💡 Route the status symbols into your sign column layout
 	symbol_position = "signcolumn",
+})
+
+-- 5. INITIALIZE LSP EXTENSION
+oil_lsp.setup({
+	count = false,
 })
